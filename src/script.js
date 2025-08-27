707b020936b85613cee7cf37aea7b3bd74a483d5
wakenedo <a_nuner@outlook.com>
2025-08-27 07:40:58 -0300

    chore(Initial_setup): Initial structure for custom npm module

--- src/script.js
+++ src/script.js
@@ -34,5 +34,30 @@ async function init() {
   await loadComponent("equipe-section", "equipe-section.html");
   await loadComponent("HTML-section", "HTML-section.html");
   await loadComponent("css-section", "css-section.html");
+
+  const { SkySystem, DynamicGradient } = window.DynamicSky;
+
+  DynamicGradient.init("#hero", {
+    type: "linear",
+    direction: "to right",
+    colors: ["#ff7e5f", "#feb47b", "#86A8E7"],
+    transitionDuration: 1500, // ms
+    schedule: [
+      { time: "6:00", colors: ["#a2ff5fff", "#FFC371", "#e9b56bff"] },
+
+      { time: "18:00", colors: ["#ff5fbcff", "#a371ffff", "#86A8E7"] },
+
+      { time: "1:00", colors: ["#fffa5fff", "#FFC371", "#e7bd86ff"] },
+    ],
+  });
+
+  SkySystem.init(document.body, {
+    withClouds: true,
+    withStars: true,
+    phase: "moon",
+    setHour: 24.5, // 14:30
+    manual: false,
+  });
 }
+
 init();
